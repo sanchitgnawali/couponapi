7d3ccfeefa9dbb8a989871c09b902cd3f1d4b013
Sanchit Gnawali <sanchitgnawali@gmail.com>
2023-10-29 21:27:09 -0500

    Init

---
+++ src/test/java/io/sanchit/projectapi/ProjectapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sanchit.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
